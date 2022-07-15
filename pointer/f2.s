5c4002b34b06baf4c57581dba590281303dff900
HunterPlus <zlw08140957@gmail.com>
2022-07-15 09:29:42 +0800

    Create f2.s

---
+++ pointer/f2.s
@@ -0,0 +1,17 @@
+#
+# void f2(int *p, int i) {
+#	p[i] = 2;
+# }
+
+	.globl f2
+	.text
+f2:
+	push	%rbp
+	mov	%rsp, %rbp
+
+	movslq	%esi, $rsi		# i - %esi, convert signed esi to rsi
+	movl	$2, (%rdi, %rsi, 4)	# p - %rdi, p[i] = p + sizeof(int) * i
+	
+	mov	%rbp, %rsp
+	pop	%rbp
+	ret
