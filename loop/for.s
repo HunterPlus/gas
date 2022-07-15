416a911dc10c7f263a132e94c999d3bc0d458d85
HunterPlus <zlw08140957@gmail.com>
2022-07-15 11:07:38 +0800

    Update for.s

--- loop/for.s
+++ loop/for.s
@@ -3,7 +3,7 @@
 # 	int 	sum, i;
 #	
 #	sum = 0;
-#	for (i = 1; i < n; i++)
+#	for (i = 1; i <= n; i++)
 #		sum += i
 #	return sum;
 # }
@@ -15,6 +15,8 @@ f:
 	mov	%rsp, %rbp
 	
 	sub	$8, %rsp
+	movl	$0, -4(%rbp)	# sum = 0
+	movl	$1, -8(%rbp)	# i = 1
 	
 	mov	%rbp, %rsp
 	pop	$rbp
