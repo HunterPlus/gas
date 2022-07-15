3faca1694c57179555b18119dc5327fb266253a1
HunterPlus <zlw08140957@gmail.com>
2022-07-15 09:55:47 +0800

    Update f4.s

--- pointer/f4.s
+++ pointer/f4.s
@@ -10,8 +10,8 @@ f4:
 	push	%rbp
 	mov	%rsp, %rbp
 		
-	movslq	$esi, $rsi
-	imul	$16, $rsi		# 16 - sizeof(struct data), i'th index
+	movslq	%esi, %rsi		# arg i - %esi
+	imul	$16, %rsi		# 16 - sizeof(struct data), i'th index
 	movl	$2, 4(%rdi, %rsi)	# p[i]->b - p + sizoef(data) * i + 4
 	
 	mov	%rbp, %rsp
