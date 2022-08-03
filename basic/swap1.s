538977ce185d346d441879ad370f1c085b85dbd8
HunterPlus <zlw08140957@gmail.com>
2022-08-03 13:00:30 +0800

    Create swap1.s

---
+++ basic/swap1.s
@@ -0,0 +1,29 @@
+/*
+void swap(int *x, int *y)
+{
+        int  temp;
+        temp = *x;
+        *x = *y;
+        *y = temp;
+}
+*/
+# %rdi - int *x
+# %rsi - int *y
+
+	.data
+	.text
+	.globl swap
+swap:
+	push	%rbp
+	mov	%rsp, %rbp
+
+	mov	(%rdi), %ebx
+	mov	(%rsi), %ecx
+	mov	%ebx, (%rsi)
+	mov	%ecx, (%rdi)	
+	
+	mov	%rbp, %rsp
+	pop	%rbp
+	ret
+	
+	
