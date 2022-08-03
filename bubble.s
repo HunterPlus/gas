85ad4c68a0d770510936fc5daa19f83fc4879ef9
HunterPlus <zlw08140957@gmail.com>
2022-08-03 10:24:31 +0800

    Update bubble.s

--- bubble.s
+++ bubble.s
@@ -11,6 +11,13 @@ void bubble(int v[], int n)
 				swap(&v[i], &v[i + 1]);
 }
 
+void printarray(int arr[], int size)
+{
+    	int i;
+    	for (i=0; i < size; i++)
+        	printf("%d ", arr[i]);
+    	printf("\n");
+}
 int main()
 {
     	int arr[] = {64, 34, 25, 12, 22, 11, 90};
@@ -18,8 +25,40 @@ int main()
 	
     	bubble(arr, n);
     	printf("Sorted array: \n");
-    	printArray(arr, n);
+    	printarray(arr, n);
 
     	return 0;
 }
 */
+
+	.data
+arr:
+    	.int 64, 34, 25, 12, 22, 11, 90
+    	.text
+fmt:
+    	.string "Sorted array: \n"
+    	.global main
+main:
+	push   	%rbp
+	mov    	%rsp, %rbp
+	
+	lea     arr(%rip), %rdi
+	mov     $7, %esi
+	call    bubble
+	
+	xor     %rax, %rax
+	lea     fmt(%rip), %rdi
+	call    printf@plt
+	
+	
+	.text
+	.globl bubble
+bubble:
+
+	.text
+	.globl printarray
+printarray:
+
+
+
+	
